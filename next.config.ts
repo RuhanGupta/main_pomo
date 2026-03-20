8d22b48dc491cedf9ddc776a96dc90c4643b5cb3
RuhanGupta <gupta56411@gapps.uwcsea.edu.sg>
2026-03-21 01:26:06 +0800

    efefe

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.88.14"]
-};
+const nextConfig: NextConfig = {};
+
+module.exports = {
+  allowedDevOrigins: ['192.168.88.14'],
+}
+
 
 export default nextConfig;
+
